b33c082da4b9a802637b1f4e846a5d447b01e19f
Gabriel Ribeiro <gribeiro8@gmail.com>
2022-12-14 14:59:37 -0300

    Class 1 development

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { PrismaService } from './prisma.service';
+
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    console.log(body);
+    const { content, category, recipientId } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
